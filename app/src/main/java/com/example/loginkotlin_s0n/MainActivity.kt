b9be9c44e1519d307fa5c299b875ce229d01a13a
s0n <shson971029@naver.com>
2022-08-11 18:52:59 +0900

    버튼 누르면 로그인 로직 구현

--- app/src/main/java/com/example/loginkotlin_s0n/MainActivity.kt
+++ app/src/main/java/com/example/loginkotlin_s0n/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.loginkotlin_s0n
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+//            아이디 입력값, 비번 입력값을 각각의 변수에 저장
+
+            val inputId = idEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+//         두개의 변수에 들어 있는 값 검사. (맞는 아이디/비번)
+
+//            맞을 때 -> 틀릴 때 분기처리. (if문 활용)
+            if (inputId== "admin@test.com" && inputPw=="qwer")
+            {
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+
+            }
+            else
+            {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                ㅁ
+            }
+
+        }
+
     }
 }
